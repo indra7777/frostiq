a2ad8a9eb96a84120d0619c4736d5cb40d23e678
Aripilli Bhavana <aripilli.bhavana8@gmail.com>
2025-07-26 05:36:04 +0530

    Updated main.py

--- BakeryBackend/main.py
+++ BakeryBackend/main.py
@@ -5,7 +5,9 @@ from routers import favorites
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Bakery Backend with Favorites")
-
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Bakery Backend API!"}
 
 app.include_router(favorites.router)
 
